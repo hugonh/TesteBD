b55263480ddaac9bf0230e36bc34ced8e80372be
Hugo Biller <hugobiller@hotmail.com>
2019-06-18 22:12:49 -0300

    Revert "Revert "Criando o Feed""
    This reverts commit 860fd59147b53cbd9df7617fbfcd9d5be647f220.

--- src/app/tabs/tabs.page.spec.ts
+++ src/app/tabs/tabs.page.spec.ts
@@ -2,6 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TabsPage } from './tabs.page';
+  
 
 describe('TabsPage', () => {
   let component: TabsPage;
