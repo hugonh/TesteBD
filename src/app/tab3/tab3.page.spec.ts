860fd59147b53cbd9df7617fbfcd9d5be647f220
Hugo Biller <hugobiller@hotmail.com>
2019-06-18 22:11:57 -0300

    Revert "Criando o Feed"
    This reverts commit 940833ccb7082fc56cb1037ca018316403095ab4.

--- src/app/tab3/tab3.page.spec.ts
+++ src/app/tab3/tab3.page.spec.ts
@@ -1,21 +1,21 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FeedPage } from './feed.page';
+import { Tab3Page } from './tab3.page';
 
-describe('Feed', () => {
-  let component: FeedPage;
-  let fixture: ComponentFixture<FeedPage>;
+describe('Tab3Page', () => {
+  let component: Tab3Page;
+  let fixture: ComponentFixture<Tab3Page>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [FeedPage],
+      declarations: [Tab3Page],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FeedPage);
+    fixture = TestBed.createComponent(Tab3Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
